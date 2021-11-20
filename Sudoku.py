3608929de275e957b3ae0424ea76e6a128bfa643
Melahen <maximinabou@gmail.com>
2021-11-20 10:20:40 +0100

    Sudoku vide modélisé

--- Sudoku.py
+++ Sudoku.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 
 #On fait un graphe avec 81 sommets et zéros arrêtes
 Sudoku = nx.empty_graph(81)
-
 # On va connecter les sommets de même ligne
 for ligne in range(0, 9) :
     # deb = 9 -> premier sommet de la deuxième ligne, deb = 8 étant le dernier sommet de la première ligne
@@ -18,9 +17,18 @@ for ligne in range(0, 9) :
 for colonne in range(0, 9) :
     for i in range(1, 9) :
         for j in range(i) :
-            Sudoku.add_edge(j * 9 + colonne, i * 9 + colonne)
-            
-            
+            Sudoku.add_edge(i * 9 + colonne, j * 9 + colonne)
+
+# Les cases de sudoku sont aussi reliées en bloc de 9, il y a 9 blocs dans un jeu de sudoku
+# Vous observerez la beauté de la présence de 9/3, 9 et 9*3 dans ce morceau de code
+for saut_colonne in range(3) :
+        for saut_ligne in range(3) :
+            deplacement = 27 * saut_colonne+ 3 * saut_ligne
+            for i in range(1,  9) :
+                for j in range(i) :
+                    u = deplacement + (i % 3) + 9 * (i // 3)
+                    v = deplacement + (j % 3) + 9 * (j // 3)
+                    Sudoku.add_edge(u, v)
 
             
             
